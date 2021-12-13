1b39344b4886b92ed1f55e8eb12cb5f8f05378f0
denis <denis@denis>
2021-12-13 16:54:36 +0300

    v1.0

---
+++ FileBackup/src/filebackup/controlers/AppProperties.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filebackup.controlers;
+
+import java.io.*;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author denis
+ */
+public class AppProperties {
+    private static AppProperties appProperties;
+    private File appFile;
+    private Properties properties;
+    private AppProperties () {
+        appFile = new File("app.prop");
+        properties = new Properties();
+        if(!appFile.exists()) try {
+            appFile.createNewFile();
+            properties.load(new FileReader(appFile));
+        } catch (IOException ex) {}
+        
+    }
+    
+    public static AppProperties getAppProperties(){
+        if (appProperties==null) appProperties=new AppProperties();
+        return appProperties;
+    }
+    
+    public String getValue (String key){
+        return properties.getProperty(key);
+    }
+    
+    public String getValue (String key, String defaultValue){
+        return properties.getProperty(key, defaultValue);
+    }
+    
+    public void setProperty (String key, String value){
+        properties.setProperty(key, value);
+        try {
+            properties.store(new FileWriter(appFile), null);
+        } catch (IOException ex) {}
+    }
+}
---
+++ FileBackup/src/filebackup/views/MainFrame.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filebackup.views;
+
+import filebackup.controlers.*;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.*;
+
+/**
+ *
+ * @author denis
+ */
+public class MainFrame extends JFrame{
+    AppProperties appProperties = AppProperties.getAppProperties();
+    
+    public MainFrame(){
+         super("File backup");
+         initFrame();
+         
+         
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                appProperties.setProperty("width", Integer.toString(getWidth()));
+                appProperties.setProperty("height", Integer.toString(getHeight()));
+                appProperties.setProperty("locationX", Integer.toString(getX()));
+                appProperties.setProperty("locationY", Integer.toString(getY()));
+                System.exit(1);
+            }
+            
+        });
+         setVisible(true);
+    }
+    
+    private void initFrame(){
+        int width = Integer.parseInt(appProperties.getValue("width", "800"));
+        int height = Integer.parseInt(appProperties.getValue("height", "600"));
+        setSize(width, height);
+        int locationX = Integer.parseInt(appProperties.getValue("locationX", "50"));
+        int locationY = Integer.parseInt(appProperties.getValue("locationY", "50"));
+        setLocation(locationX, locationY);
+    }
+}
